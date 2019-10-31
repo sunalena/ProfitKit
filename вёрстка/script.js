dba6337a8ad36b766e098c2a5033661f2aac0762
sunalena <33164590+sunalena@users.noreply.github.com>
2019-10-31 18:12:42 +0300

    Add files via upload

---
+++ вёрстка/script.js
@@ -0,0 +1,8 @@
+function open_menu() {
+  element = document.getElementById("submenu")
+  if (element.style.display == "none") {
+    element.style.display = "block"
+  } else {
+    element.style.display = "none"
+  }
+}
